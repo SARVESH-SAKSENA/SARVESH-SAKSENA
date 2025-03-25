e613ca7268c5e884747e2c48d9707d1a83f4e360
Sarvesh-Saksena53 <sarvsak10@gmail.com>
2025-03-25 19:57:54 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
+import React from 'react';
+
+import ReactDOM from'react-dom/client';
+
 const parent = React.createElement("div" , { id : "parent"} , [
-    React.createElement("div" , {id : "child1"} , React.createElement("h1" , {} , "I'm h1 tag")),
+    React.createElement("div" , {id : "child1"} , React.createElement("h1" , {} , "Namaste React")),
     React.createElement("div" , {id : "child1"} , React.createElement("h1" , {} , "I'm h1 tag"))
 ]);
 
